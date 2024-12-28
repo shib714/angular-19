ccb9bcf273b6ffa42d33151daf2b80cad7a87c57
shib7 <shib714@gmail.com>
2024-12-27 23:08:46 -0500

    3nd Commit

--- src/app/components/designation/designation.component.ts
+++ src/app/components/designation/designation.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, resource } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { IUser } from '../../model/interface/user';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-designation',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './designation.component.html',
   styleUrl: './designation.component.css'
 })
-export class DesignationComponent {
+export class DesignationComponent  implements OnInit {
 
-}
+  users:IUser [] = [];
+  usersResource: any;
+  url: string ="http://localhost:3000/users";
+  //http = inject(HttpClient);
+
+  constructor(private http: HttpClient) {
+    this.getUsers();
+  }
+
+
+  ngOnInit(): void {
+    //throw new Error('Method not implemented.');
+  }
+
+  //traditional http client
+  // getUsers() {
+  //   this.http.get(this.url).subscribe((res) => {
+  //     this.users = res;
+  //     console.log(this.users);
+  //   })
+  // }
+
+  //using rxResource in angular 19
+  getUsers() {
+    this.usersResource = rxResource( {
+      loader: () => {
+        return this.http.get(this.url); //just return observer, no need to subscribe
+      }
+    });
+  }
+
+  //using resource
+//     getUsers() {
+//     this.usersResource = resource( {
+//       loader: () => {
+//         return fetch(this.url).then(res => res.json); 
+//       }
+//     });
+//   }
+ }
--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,29 +1,89 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IRole } from '../../model/interface/role';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-roles',
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
-export class RolesComponent {
- //data types -> string, number, boolean, date, object, array, null, undefined
+export class RolesComponent  {
+[x: string]: any;
 
- firstName:string ='Angular 19';
- version: number= 19;
- 
- isActive:boolean = false;
+  url: string = "http://localhost:3000/roles";
 
- currentDate: Date = new Date();
+  roles: IRole[] = [];
 
- selectedState: string ='';
+  rolesResource:any;
 
- showWelcomeMessage() : void{
-  alert("Welcome to the Angular 19 Tutorial");
- }
+  //http = inject(HttpClient);
 
- showWelcomeWithParameter(msg: string) {
-  alert(msg);
- }
+  
+   constructor(private http: HttpClient) {
+    this.getAllRoles();
+    }
+
+
+  ngOnInit(): void {
+    //this.getAllRoles();
+  }
+
+  // getAllRoles() {
+  //   this.http.get(this.url).subscribe((res:IRole) => {
+  //     this.rolesList = res.data;
+  //   });
+
+
+  //using rxResource
+  getAllRoles() {
+    rxResource({
+      loader: () => {
+        return this.http.get(this.url);
+      }
+    });
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //data types -> string, number, boolean, date, object, array, null, undefined
+
+  //  firstName:string ='Angular 19';
+  //  version: number= 19;
+
+  //  isActive:boolean = false;
+
+  //  currentDate: Date = new Date();
+
+  //  selectedState: string ='';
+
+  //  showWelcomeMessage() : void{
+  //   alert("Welcome to the Angular 19 Tutorial");
+  //  }
+
+  //  showWelcomeWithParameter(msg: string) {
+  //   alert(msg);
+  //  }
+
+
+
 }
